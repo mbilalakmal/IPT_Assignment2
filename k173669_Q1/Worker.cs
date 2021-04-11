aaa07328450de89260ef7989f3b05d2b9bc8ec96
mbilalakmal <37498621+mbilalakmal@users.noreply.github.com>
2021-04-11 19:43:51 +0500

    Added hardcoded exe path and command line arguments.

--- k173669_Q1/Worker.cs
+++ k173669_Q1/Worker.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,8 +22,26 @@ namespace k173669_Q1
         {
             while (!stoppingToken.IsCancellationRequested)
             {
+                try
+                {
+                    using Process process = new();
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.CreateNoWindow = true;
+
+                    process.StartInfo.FileName = @"C:\Users\bilal\source\repos\IPT_Assignment1\k173669_Q1\bin\Release\netcoreapp3.1\k173669_Q1.exe";
+                    process.StartInfo.Arguments = "https://www.psx.com.pk/market-summary/ C:\\Users\\bilal\\Downloads";
+
+                    process.Start();
+                }
+                catch (Exception e)
+                {
+
+                    /// TODO: Log the exception
+                    Console.WriteLine(e.Message);
+                }
+
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(10000, stoppingToken);
             }
         }
     }
