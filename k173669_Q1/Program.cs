53c0f7377fe796a5fc21e68e9fc0bdf093291ec5
mbilalakmal <37498621+mbilalakmal@users.noreply.github.com>
2021-04-13 17:53:25 +0500

    Added Serilog and configure to ouput logs to Documents/LogFile.txt

--- k173669_Q1/Program.cs
+++ k173669_Q1/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
 
 namespace k173669_Q1
 {
@@ -7,12 +10,25 @@ namespace k173669_Q1
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.File(Path.Combine(
+                    Environment.GetFolderPath( Environment.SpecialFolder.MyDocuments), "LogFile.txt")
+                )
+                .CreateLogger()
+                ;
+
             CreateHostBuilder(args).Build().Run();
+
+            Log.CloseAndFlush();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .UseWindowsService()
+                .UseSerilog()
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.Configure<DownloadConfig>(hostContext.Configuration.GetSection("AppSettings"));
