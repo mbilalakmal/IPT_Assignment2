68883c305524b5d734e61b63d9a19498d84e94e5
mbilalakmal <37498621+mbilalakmal@users.noreply.github.com>
2021-04-14 17:11:06 +0500

    Added WorkerService project for Q4 and added packages

---
+++ k173669_Q4/MergeProcess.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace k173669_Q4
+{
+    public class MergeProcess
+    {
+        private readonly MergeConfig config;
+        private readonly ILogger<MergeProcess> logger;
+
+        public MergeProcess(IOptions<MergeConfig> config, ILogger<MergeProcess> logger)
+        {
+            this.config = config.Value;
+            this.logger = logger;
+        }
+
+    }
+
+    public class MergeConfig
+    {
+        public string InputDirectory { get; set; }
+        public string OutputDirectory { get; set; }
+        public int IntervalBetween { get; set; }
+
+    }
+}
---
+++ k173669_Q4/MergeWorker.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace k173669_Q4
+{
+    public class MergeWorker : BackgroundService
+    {
+        private readonly ILogger<MergeWorker> _logger;
+
+        public MergeWorker(ILogger<MergeWorker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
