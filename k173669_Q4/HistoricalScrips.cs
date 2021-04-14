a6af95898952b0ae6b0f309b087916a7d111832e
mbilalakmal <37498621+mbilalakmal@users.noreply.github.com>
2021-04-15 00:46:31 +0500

    Added Scrips and HistoricalScrips models

---
+++ k173669_Q4/HistoricalScrips.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace k173669_Q4
+{
+    public class HistoricalScrips
+    {
+
+        public string Scrip { get; set; }
+        public DateTime LastUpdatedOn { get; set; }
+        public List<PriceSnapshot> ScripData { get; set; } = new();
+    }
+
+    public struct PriceSnapshot
+    {
+        public DateTime Date { get; set; }
+        public double Price { get; set; }
+    }
+
+}
---
+++ k173669_Q4/Scrips.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace k173669_Q4
+{
+    [Serializable]
+    public class Scrips
+    {
+        public string Category { get; set; }
+        public string Scrip { get; set; }
+        public double Price { get; set; }
+    }
+}
